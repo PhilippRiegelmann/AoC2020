606c432811d9e83ccaa477fa922b32a9493d4e48
PhilippRiegelmann <PhilippRiegelmann@users.noreply.github.com>
2020-12-13 13:36:40 +0100

    init

--- 13/Advent_of_Code_13.py
+++ 13/Advent_of_Code_13.py
@@ -14,6 +14,16 @@ def in_service(input_string):
     return list_in_service
 
 
+def in_service_and_offset(input_string):
+    list_in_service = []
+    input_string_list = input_string.split(",")
+    for i in range(len(input_string_list)):
+        ID_and_offset = []
+        if input_string_list[i] != "x":
+            list_in_service.append([int(input_string_list[i]), i])
+    return list_in_service
+
+
 def min_minutes_to_wait(time, busses_on_service):
     time_to_wait = 100000000000000
     bus_ID_to_take = 0
@@ -31,6 +41,8 @@ def main():
     available_buses = in_service(input[1])
     time_to_wait, bus_to_take = min_minutes_to_wait(earliest_timestamp, available_buses)
     print(time_to_wait * bus_to_take)
+    print("")
+    print(in_service_and_offset(input[1]))
 
 
 if __name__ == "__main__":
